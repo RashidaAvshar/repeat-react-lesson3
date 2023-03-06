f0c69d7e1651a96e40498756faa4076dd2ba803e
RashidaAvshar <rashidaavshar@gmail.com>
2023-03-06 16:13:38 +0400

    table

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,13 +3,40 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import axios from 'axios';
+import { BASE_URL } from '../utilis/constans';
+import swal from 'sweetalert';
 
 
-const onHandleClick = ()=>{
-    console.log("click")
-}
 
 const Register = () => {
+    const [userData, setUserData] = useState({
+        name: "",
+        surname: "",
+        username: "",
+        password: "",
+        id: Date.now()
+    })
+
+    const onHandleClick = ()=>{
+        axios.post(`${BASE_URL}/create-data`, userData).then((res)=>{
+            if(res.status === 201){
+        swal("Good job!", "You clicked the button!", "success");
+            }
+
+        })
+        {
+            document.querySelector("#name").value="";
+            document.querySelector("#surname").value="";
+            document.querySelector("#username").value="";
+            document.querySelector("#password").value="";
+          }
+    }
+    const onHandleChange = (e)=>{
+        setUserData({...userData,[e.target.name]:e.target.value})
+    }
+
+    const {name, surname, username, password} = userData
   return (
     <div className='container'>
         <div className="register-box">
@@ -22,14 +49,14 @@ const Register = () => {
       noValidate
       autoComplete="off"
     >
-      <TextField id="name" label="name" variant="outlined" />
-      <TextField id="surname" label="surname" variant="outlined" />
-      <TextField id="username" label="username" variant="outlined" />
-      <TextField id="password" label="password" variant="outlined" type={'password'}  />
+      <TextField id="name" label="name" variant="outlined" onChange={onHandleChange} />
+      <TextField id="surname" label="surname" variant="outlined" onChange={onHandleChange} />
+      <TextField id="username" label="username" variant="outlined" onChange={onHandleChange}  />
+      <TextField id="password" label="password" variant="outlined" type={'password'} onChange={onHandleChange}  />
       
     </Box>
     <Stack spacing={2} direction="row">
-      <Button variant="register" onClick={onHandleClick}>register</Button>
+      <Button variant="register" onClick={onHandleClick} >register</Button>
     </Stack>
         </div>
       
